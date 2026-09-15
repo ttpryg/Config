87a37cb6e28892f8cafc4d0449f0c5fd19cca9e1
Toto <totoprayogo1916@gmail.com>
2026-09-15 09:18:46 +0700

    fix: add rector & pint (#5)
    * refactor: clean up README and composer.json by removing unused database driver and adding support section
    
    * refactor: enhance CI workflow by adding Rector & Pint checks and update composer.json for new scripts and dependencies
    
    * fix: downgrade PHP version from 8.2 to 8.1 in Dockerfile and docker-compose.yml
    
    * feat: add rector configuration for code quality checks and update .gitignore
    
    * refactor: clean up ConfigRepository by removing unnecessary blank lines

--- src/ConfigRepository.php
+++ src/ConfigRepository.php
@@ -11,17 +11,9 @@ use Ttpryg\Config\Drivers\DatabaseDriverInterface;
 class ConfigRepository implements ArrayAccess, ConfigInterface
 {
     /**
-     * @var array<string, mixed>
-     */
-    protected array $items = [];
-
-    /**
      * @param  array<string, mixed>  $items
      */
-    public function __construct(array $items = [])
-    {
-        $this->items = $items;
-    }
+    public function __construct(protected array $items = []) {}
 
     public function get(string $key, mixed $default = null): mixed
     {
@@ -55,6 +47,7 @@ class ConfigRepository implements ArrayAccess, ConfigInterface
             if (! isset($array[$segment]) || ! is_array($array[$segment])) {
                 $array[$segment] = [];
             }
+
             $array = &$array[$segment];
         }
 
@@ -93,6 +86,7 @@ class ConfigRepository implements ArrayAccess, ConfigInterface
             if (! isset($array[$segment]) || ! is_array($array[$segment])) {
                 return;
             }
+
             $array = &$array[$segment];
         }
 
